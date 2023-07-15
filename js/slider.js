e0ae19ff1413f634590177140a1729fd456b4382
cifrasre <cifrasreligiosas@gmail.com>
2023-07-15 09:44:31 -0300

    asc

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+function Um(){
+    document.getElementById('visor').style.backgroundImage = "url('../img/slider/1.jpg')";
+}
+
+function Dois(){
+    document.getElementById('visor').style.backgroundImage = "url('../img/slider/2.jpg')";
+}
+
+function Tres(){
+    document.getElementById('visor').style.backgroundImage = "url('../img/slider/3.jpg')";
+}
+
+function Quatro(){
+    document.getElementById('visor').style.backgroundImage = "url('../img/slider/4.jpg')";
+}
